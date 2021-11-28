ab3ef7d82094ea22f0ec5c0e53eab3ecb09f00d2
Manvendra Singh <mjdeora@gmail.com>
2021-11-28 21:36:34 +0530

    Data fetching: react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
-import "./App.css";
+import { API_KEY } from "./config";
+import { useQuery } from "react-query";
 
 function App() {
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch(API_KEY).then((res) => {
+      return res.json();
+    })
+  );
+
+  if (isLoading) {
+    return "Loading...";
+  }
+
+  if (error) {
+    console.error(error);
+    return "Error";
+  }
+
+  console.log({ data });
+
   return <div className="App">Admin UI</div>;
 }
 
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
