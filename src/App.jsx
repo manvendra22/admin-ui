8787206de1ca9427bcc05d3e99883ecb7c7217b5
Manvendra Singh <mjdeora@gmail.com>
2021-12-01 17:53:16 +0530

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ const App = () => {
 
   if (isLoading) {
     return (
-      <div class="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-screen">
         <div
-          class="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500
+          className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500
     "
         ></div>
       </div>
